5b09db26e1544d3e3c589b3c6af36c6a104756e5
Trevor Jobling <trevor.jobling@dovetail.ie>
2024-12-19 14:18:09 +0000

    Support Bottle 0.13

--- bottle_flash2.py
+++ bottle_flash2.py
@@ -1,10 +1,11 @@
+import json
 from functools import wraps
 from bottle import request, response
 
 class FlashPlugin(object):
     name = 'flash'
 
-    def __init__(self, key='flash', secret=None):
+    def __init__(self, key='bottleflash2', secret=None):
         self.key = key
         self.secret = secret
         self.app = None
@@ -15,28 +16,30 @@ class FlashPlugin(object):
         self.app.add_hook('after_request', self.set_flashed)
         self.app.flash = self.flash
         self.app.get_flashed_messages = self.get_flashed_messages
-
+        
     def load_flashed(self):
-        m = request.get_cookie(self.key, self.secret)
+        m = request.get_cookie(key=self.key, secret=self.secret)
         if m is not None:
-            response.flash_messages = m
+            response.flash_messages = json.loads(m)
 
     def set_flashed(self):
         if hasattr(response, 'flash_messages'):
-            response.set_cookie(self.key, response.flash_messages, self.secret)
+            flash_messages_json = json.dumps(response.flash_messages)
+            response.set_cookie(self.key, flash_messages_json, self.secret)
             delattr(response, 'flash_messages')
 
     def flash(self, message, level=None):
         if not hasattr(response, 'flash_messages'):
             response.flash_messages = []
-        response.flash_messages.append((message, level))
+        flash_data = json.dumps({'message': message, 'level': level})
+        response.flash_messages.append(flash_data)
 
     def get_flashed_messages(self):
         if hasattr(response, 'flash_messages'):
             m = response.flash_messages
             delattr(response, 'flash_messages')
             response.delete_cookie(self.key)
-            return m
+            return [json.loads(flash_data) for flash_data in m]
             
     def apply(self, callback, context):
         return callback
